8d4559fe482ddc2f48f65ed6f45b78f25196353b
Mangesh636 <mangeshbhardwaj007@gmail.com>
2024-09-15 21:37:28 +0530

    Add path alias for source directory in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+import path from 'path'
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: "./",
 })
